9127cc178449abfef42f01831cbb72da84f4706d
wenxi1608 <wenxihe1@gmail.com>
2022-10-29 13:53:00 +0800

    chore: updated backend URL

--- src/apis/watchlist.js
+++ src/apis/watchlist.js
@@ -4,6 +4,7 @@ const apis = {
   addToWatchlist: async (projectName, token) => {
     const response = await axios.post(
       `${process.env.REACT_APP_BACKEND_URL}/api/watchlist/${projectName}`,
+      {},
       {
         headers: {
           Authorization: token,
